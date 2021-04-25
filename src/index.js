2bb9ad51b2af62cfe0a3b3ea63091a62e5fc63da
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-04-25 13:42:59 +0530

    basic if condition with variable

--- src/index.js
+++ src/index.js
@@ -11,16 +11,25 @@ const img1 = "https://picsum.photos/200/200";
 const img2 = "https://picsum.photos/300/200";
 const img3 = "https://picsum.photos/100/200";
 
+let heading = {
+  textShadow: "2px 2px 0 black",
+  fontWeight: "600"
+};
+
+let currDate = new Date(2021, 4, 1, 17).getHours();
+
+let greeting = "Good morning";
+
+if (currDate >= 12 && currDate < 16) {
+  greeting = "Good afternoon";
+} else if (currDate >= 16 && currDate <= 24) {
+  greeting = "Good evening";
+}
+
 ReactDOM.render(
   <>
-    <h1
-      className="title"
-      style={{
-        color: "Red",
-        fontWeight: "600"
-      }}
-    >
-      Something
+    <h1 className="title" style={heading}>
+      Hi Robin, {greeting}
     </h1>
     <p>The date is {date}</p>
     <p>The date is {time}</p>
