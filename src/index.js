d07c4795f3d00e1b55335f91647a89ae0d49c399
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-04-25 13:19:11 +0530

    classname and basic css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "./index.css";
 // import App from "./App";
 
 const rootElement = document.getElementById("root");
 let date = new Date().toLocaleDateString();
 let time = new Date().toLocaleTimeString();
 
+const img1 = "https://picsum.photos/200/200";
+const img2 = "https://picsum.photos/300/200";
+const img3 = "https://picsum.photos/100/200";
+
 ReactDOM.render(
   <>
-    <h1>Something</h1>
+    <h1 className="title">Something</h1>
     <p>The date is {date}</p>
     <p>The date is {time}</p>
+    <img src={img1} alt="example" />
+    <img src={img2} alt="example" />
+    <img src={img3} alt="example" />
   </>,
   rootElement
 );
