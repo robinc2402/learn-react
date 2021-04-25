f8ffb848c1479aeedfa83fbd55429983e6a8240e
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-04-25 15:59:55 +0530

    Map function general usage

--- src/CardGrid.jsx
+++ src/CardGrid.jsx
@@ -3,39 +3,19 @@ import Card from "./Card";
 import "./assets/css/cards.css";
 import Sdata from "./CardData";
 
-console.log(Sdata);
+function ncard(val) {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.snam}
+      link={val.link}
+    />
+  );
+}
 
 function CardGrid() {
-  return (
-    <>
-      <div className="cards">
-        <Card
-          imgsrc={Sdata[0].imgsrc}
-          title={Sdata[0].title}
-          sname={Sdata[0].snam}
-          link={Sdata[0].link}
-        />
-        <Card
-          imgsrc={Sdata[1].imgsrc}
-          title={Sdata[1].title}
-          sname={Sdata[1].snam}
-          link={Sdata[1].link}
-        />
-        <Card
-          imgsrc={Sdata[2].imgsrc}
-          title={Sdata[2].title}
-          sname={Sdata[2].snam}
-          link={Sdata[2].link}
-        />
-        <Card
-          imgsrc={Sdata[3].imgsrc}
-          title={Sdata[3].title}
-          sname={Sdata[3].snam}
-          link={Sdata[3].link}
-        />
-      </div>
-    </>
-  );
+  return <div className="cards">{Sdata.map(ncard)}</div>;
 }
 
 export default CardGrid;
