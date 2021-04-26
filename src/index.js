c7dd3f6331843570c7af37ae7ded4c319fc7ef8d
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-04-26 23:33:59 +0530

    Array/Object destructuring with String literals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./slotmachine/App";
-ReactDOM.render(<App />, document.getElementById("root"));
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./slotmachine/App";
+// ReactDOM.render(<App />, document.getElementById("root"));
+let MyArr = ["noodles", "rice", "taco", "bread", "sauce", "butter"];
+
+let [a, , , c] = MyArr;
+
+console.log(`I want to eat ${c} with ${a} on top of it.`);
